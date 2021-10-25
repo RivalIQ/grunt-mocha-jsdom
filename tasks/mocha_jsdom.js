0e56f2ffd2f122cae055dc18d1fe3685ec6f7aa8
Seth Pollack <seth@pollackphoto.com>
2021-10-25 16:33:58 -0700

    falsy check

--- tasks/mocha_jsdom.js
+++ tasks/mocha_jsdom.js
@@ -98,12 +98,8 @@ module.exports = function(grunt) {
                         grunt.file.write(path.resolve(options.outfile), html);
                     }
 
-                    var stats = reporter.stats;
-                    if (!stats) {
-                        grunt.log.writeln('stats object missing!');
-                    }
-
                     process.nextTick(function () {
+                        var stats = reporter.stats;
                         if (stats && stats.failures > 0) {
                             grunt.fail.fatal(stats.failures + ' test failure(s)', 1);
                         }
